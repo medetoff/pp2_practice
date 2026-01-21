dc65eb7930dd172d0783945e0966edad0607452d
medetoff <medetmusirepov77@gmail.com>
2026-01-21 16:24:55 +0500

    Add Practice Python examples

---
+++ practice/comments.py
@@ -0,0 +1,14 @@
+#This is a comment 1
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment 2
+
+#print("Hello, World!") 3
+print("Cheers, Mate!")
+
+"""
+This is a comment
+written in
+more than just one line 4
+"""
+print("Hello, World!")
---
+++ practice/variables.py
@@ -0,0 +1,33 @@
+#1 Example
+x = 5
+y = "John"
+print(x)
+print(y)
+#2 Example
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x) 
+
+#3 Example
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#4 Example You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#example 5 Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+#Example 6 Python allows you to assign values to multiple variables in one line:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
